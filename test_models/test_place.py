82224288051a716338e9dda88aaf4badeb2c1e94
raebrown-ctrl <brownrae6@gmail.com>
2023-06-11 18:03:25 -0700

    test place 2

--- test_models/test_place.py
+++ test_models/test_place.py
@@ -1,1 +1,33 @@
-#!/bin/usr/Python3
+#!/usr/bin/python3
+"""
+uinitest for the City
+"""
+
+
+
+
+from models.place import Place
+from datetime import datetime
+from time import sleep
+import unittest
+import uuid
+
+class Test_Place(unittest.TestCase):
+    """
+    calss Test for Place
+    """
+     
+    def test_city_id(self):
+        self.assertEqual(str, type(Place.city_id))
+
+    def test_name(self):
+        self.assertEqual(str, type(Place.name))
+
+    def test_number_rooms(self):
+        self.assertEqual(int, type(Place.number_rooms))
+
+    def test_latitude(self):
+        self.assertEqual(float, type(Place.latitude))
+    
+    def test_amenity_ids(self):
+        self.assertEqual(list, type(Place.amenity_ids))
