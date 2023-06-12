31d4ebd04cb05819b150d135523a28188f22a4c6
Jamie897 <114971783+Jamie897@users.noreply.github.com>
2023-06-12 09:07:17 -0500

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,28 +6,27 @@ import json
 from models.base_model import BaseModel
 import os
 from models.user import User
-from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.amenity import Amenity
 
-import json
-import os.path
+"""
+    FileStorage class file
+"""
 
 
 class FileStorage:
     """
-    class FileStorage that serializes instances to a JSON file
-    and deserializes JSON file to instances:
+        class FileStorage
     """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """
-        public instance that returns the dictionary objects
+            returns the dictionary __objects
         """
         return self.__objects
 
