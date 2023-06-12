67f6b7bff99696442bc9cedf2a8667111a76776d
Jamie897 <114971783+Jamie897@users.noreply.github.com>
2023-06-12 09:02:37 -0500

    fixed

--- models/state.py
+++ models/state.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
+"""
+    import module
+"""
+from models.base_model import BaseModel
+
+
+"""
+    State class File
+"""
+
 
 class State(BaseModel):
     """
--- models/user.py
+++ models/user.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 """
-hbnb clone
+    import module
 """
 from models.base_model import BaseModel
 
+
+"""
+    User class File
+"""
+
+
+class User(BaseModel):
+    """
+        class User inherits from BaseModel
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
