000c2134e808361ef150ae535b78afed7cd39b25
Jamie897 <114971783+Jamie897@users.noreply.github.com>
2023-06-11 20:58:05 -0500

    city.py

---
+++ models/city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    import module
+"""
+from models.base_model import BaseModel
+
+
+"""
+    City class File
+"""
+
+
+class City(BaseModel):
+    """
+        City class inherit from BaseModel
+    """
+    name = ""
+    state_id = ""
