221bd8b634a859f855b6f009e1314d80f9e64c7c
Jamie897 <114971783+Jamie897@users.noreply.github.com>
2023-06-11 20:35:19 -0500

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -13,8 +13,6 @@ class BaseModel():
     Class BaseModel that define all commun
     attribute methods for other class
     """
-    
-
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
